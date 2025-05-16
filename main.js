3465080b88b977aa4160fc5b2386c3174fad9202
rafaelapizzolotto <164499957+rafaelapizzolotto@users.noreply.github.com>
2025-05-16 08:21:03 -0300

    .

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-01-20T00:00:00");
+const tempoObjetivo1 = new Date("2026-03-10T00:00:00");
 const tempoObjetivo2 = new Date("2026-04-24T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-19T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-31T00:00:00");
